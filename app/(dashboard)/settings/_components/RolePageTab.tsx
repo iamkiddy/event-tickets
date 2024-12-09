bcb66359cdf7c65ebc0ae43aa29bf889f511b85b
Denzel Hayford <dhayford@gmail.com>
2024-12-08 19:19:06 -1200

    Refactor dashboard and profile components; add settings pages and new tab selector

---
+++ app/(dashboard)/settings/_components/RolePageTab.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RolePageTab() {
+  return (
+    <div>RolePageTab</div>
+  )
+}
---
+++ app/(dashboard)/settings/_components/TeamPageTab.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TeamPageTab() {
+  return (
+    <div>TeamPageTab</div>
+  )
+}
