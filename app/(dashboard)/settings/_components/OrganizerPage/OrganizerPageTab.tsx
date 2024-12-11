0e04af8c8a6bb7f776018154d4337a3d9043bc8e
Denzel Hayford <dhayford@gmail.com>
2024-12-10 13:27:54 -1200

    Refactor OrganizerPageTab component: update "use client" directive and simplify layout structure; remove unused SearchField

--- app/(dashboard)/settings/_components/OrganizerPage/OrganizerPageTab.tsx
+++ app/(dashboard)/settings/_components/OrganizerPage/OrganizerPageTab.tsx
@@ -1,6 +1,5 @@
-'use client'
+"use client"
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import SearchField from '@/components/custom/SearchField'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import {Sheet, SheetTrigger } from "@/components/ui/sheet"
@@ -19,17 +18,14 @@ export default function OrganizerPageTab() {
 
   return (
     <section className='w-full flex flex-col gap-4'>
-        <div className='w-full flex flex-col gap-3 md:flex-row items-center justify-end'>
-          <SearchField />
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button className='h-[40px] md:px-5 rounded-md w-full md:w-fit'>
-                Add organizer profile
-              </Button>
-            </SheetTrigger>
-            <AddOrgPage />
-          </Sheet>
-        </div>
+        <Sheet>
+          <SheetTrigger asChild className='ml-auto'>
+            <Button className='h-[40px] md:px-5 rounded-md w-full md:w-fit'>
+              Add organizer profile
+            </Button>
+          </SheetTrigger>
+          <AddOrgPage />
+        </Sheet>
 
         {isLoading ? (
           <PageLoader />
@@ -38,7 +34,6 @@ export default function OrganizerPageTab() {
             <OrgCard key={org.id} data={org} />
           ))
         )}
-        
     </section>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,11 @@ import "./globals.css";
 import { AuthProvider } from '@/lib/context/AuthContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { Toaster } from 'sonner';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
 export const metadata: Metadata = {
   title: "CodePass",
   description: "CodePass",
 };
-
-
-const queryClient = new QueryClient();
 
 
 export default function RootLayout({
@@ -22,18 +18,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <QueryClientProvider client={queryClient}>
-          <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
-            <AuthProvider>
-                {children}
-            </AuthProvider>
-          </GoogleOAuthProvider>
-          <Toaster 
-            position="top-right"
-            richColors
-            closeButton
-          />
-        </QueryClientProvider>
+        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}>
+          <AuthProvider>
+              {children}
+          </AuthProvider>
+        </GoogleOAuthProvider>
+        <Toaster 
+          position="top-right"
+          richColors
+          closeButton
+        />
       </body>
     </html>
   );
