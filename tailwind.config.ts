4e910d10124d28f5c684315248f7e7cce87cbc1b
iamkiddy <francisyoungson@gmail.com>
2024-12-16 10:08:07 -0600

    Added and fixed errors in the main home

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,11 @@ export default {
 				"0%,70%,100%": { opacity: "1" },
 				"20%,50%": { opacity: "0" },
 			},
+      "shimmer": {
+        "100%": {
+          "transform": "translateX(100%)",
+        },
+      },
 		},
 		animation: {
 			"caret-blink": "caret-blink 1.25s ease-out infinite",
